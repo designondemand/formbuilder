82115303a656db57cd828a0a48049bf2e2378484
sjg <sjg@f3358df3-8306-0410-9a17-d642f5d875db>
2006-05-29 04:01:09 +0000

    Added Multi-select field type

---
+++ classes/MultiselectField.class.php
@@ -0,0 +1,170 @@
+<?php
+/* 
+   FormBuilder. Copyright (c) 2005-2006 Samuel Goldstein <sjg@cmsmodules.com>
+   More info at http://dev.cmsmadesimple.org/projects/formbuilder
+   
+   A Module for CMS Made Simple, Copyright (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
+  This project's homepage is: http://www.cmsmadesimple.org
+*/
+
+class fbMultiselectField extends fbFieldBase {
+
+	var $optionCount;
+	var $optionAdd;
+
+	function fbMultiselectField(&$form_ptr, &$params)
+	{
+       $this->fbFieldBase($form_ptr, $params);
+        $mod = $form_ptr->module_ptr;
+		$this->Type = 'MultiselectField';
+		$this->DisplayInForm = true;
+		$this->NonRequirableField = false;
+		$this->HasAddOp = true;
+		$this->HasDeleteOp = true;
+		$this->ValidationTypes = array(
+            );
+        $this->optionAdd = 0;
+	}
+
+	function GetOptionAddButton()
+	{
+		$mod = $this->form_ptr->module_ptr;
+		return $mod->Lang('add_options');
+	}
+
+	function GetOptionDeleteButton()
+	{
+		$mod = $this->form_ptr->module_ptr;
+		return $mod->Lang('delete_options');
+	}
+
+	function DoOptionAdd(&$params)
+	{
+		$this->optionAdd = 2;
+	}
+
+	function DoOptionDelete(&$params)
+	{
+		$delcount = 0;
+		foreach ($params as $thisKey=>$thisVal)
+			{
+			if (substr($thisKey,0,4) == 'del_')
+				{
+				$this->RemoveOptionElement('option_name', $thisVal - $delcount);
+				$this->RemoveOptionElement('option_value', $thisVal - $delcount);
+				$delcount++;
+				}
+			}
+	}
+
+	function countItems()
+	{
+			$tmp = &$this->GetOptionRef('option_name');
+			if (is_array($tmp))
+				{
+	        	$this->optionCount = count($tmp);
+	        	}
+	        elseif ($tmp !== false)
+	        	{
+	        	$this->optionCount = 1;
+	        	}
+	        else
+	        	{
+	        	$this->optionCount = 0;
+	        	}
+	}
+
+	function GetFieldInput($id, &$params, $returnid)
+	{
+		$mod = $this->form_ptr->module_ptr;
+
+		// why all this? Associative arrays are not guaranteed to preserve
+		// order, except in "chronological" creation order.
+		$sorted =array();
+		$subjects = &$this->GetOptionRef('option_name');
+
+		if (count($subjects) > 1)
+			{
+			for($i=0;$i<count($subjects);$i++)
+				{
+				$sorted[$subjects[$i]]=($i+1);
+				}
+			}
+		else
+			{
+			$sorted[$subjects] = '1';
+			}
+		return $mod->CreateInputSelectList($id, '_'.$this->Id.'[]', $sorted,($this->Value!==false?$this->Value:array()) , $this->GetOption('lines','3'));
+	}
+
+
+
+    function StatusInfo()
+	{
+		$mod = $this->form_ptr->module_ptr;
+		$opt = $this->GetOption('option_name','');
+		
+		if (is_array($opt))
+		  {
+		      $num = count($opt);
+		  }
+		elseif ($opt != '')
+			{
+			$num = 1;
+			}
+		else
+		  {
+          $num = 0;
+          }
+         $ret= $mod->Lang('options',$num);
+        return $ret;
+	}
+	
+	function PrePopulateAdminForm($formDescriptor)
+	{
+		$mod = $this->form_ptr->module_ptr;
+
+		$this->countItems();
+		if ($this->optionAdd > 0)
+			{
+			$this->optionCount += $this->optionAdd;
+			$this->optionAdd = 0;
+			}
+		$dests = '<table class="module_fb_table"><tr><th>'.$mod->Lang('title_option_name').'</th><th>'.
+			$mod->Lang('title_option_value').'</th><th>'.
+			$mod->Lang('title_delete').'</th></tr>';
+
+
+		for ($i=0;$i<($this->optionCount>1?$this->optionCount:1);$i++)
+			{
+			$dests .=  '<tr><td>'.
+            		$mod->CreateInputText($formDescriptor, 'opt_option_name[]',$this->GetOptionElement('option_name',$i),25,128).
+            		'</td><td>'.
+            		$mod->CreateInputText($formDescriptor, 'opt_option_value[]',$this->GetOptionElement('option_value',$i),25,128).
+            		'</td><td>'.
+            		$mod->CreateInputCheckbox($formDescriptor, 'del_'.$i, $i,-1).
+             		'</td></tr>';
+			}
+		$dests .= '</table>';
+		$main = array();
+		$adv = array();
+		array_push($main,array($mod->Lang('title_lines_to_show'),$mod->CreateInputText($formDescriptor, 'opt_lines',$this->GetOption('lines','3'),10,10)));
+		array_push($main,array($mod->Lang('title_multiselect_details'),$dests));
+		return array('main'=>$main,'adv'=>$adv);
+	}
+
+	function GetHumanReadableValue()
+	{
+		$mod = $this->form_ptr->module_ptr;
+		if ($this->HasValue())
+			{
+			return $this->GetOptionElement('option_value',($this->Value-1));
+			}
+		else
+			{
+			return $mod->Lang('unspecified');
+			}	
+	}
+	
+}
+?>
