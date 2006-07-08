62444887765ff6d6f0c2beea149d1f1a8975677e
sjg <sjg@f3358df3-8306-0410-9a17-d642f5d875db>
2006-07-08 04:36:38 +0000

    Cleanup response_id code

--- FormBuilder.module.php
+++ FormBuilder.module.php
@@ -333,6 +333,88 @@ class FormBuilder extends CMSModule
 			}
 		return array($records, $names, $values);
 	}
+	
+/*	
+	function HandleSubmission(&$params, $targetpage, $side='user', $hideError=true)
+	{
+        if (! isset($params['form_id']) && isset($params['form']))
+            {
+            // get the form by name, not ID
+            $params['form_id'] = $this->GetFormIDFromAlias($params['form']);
+            }
+        $aeform = new fbForm($this,$params,true);
+
+        echo $aeform->RenderFormHeader();
+        $finished = false;
+        if (($aeform->GetPageCount() > 1 && $aeform->GetPageNumber() > 0) ||
+        	(isset($params['done'])&& $params['done']==1))
+            {
+        	$res = $aeform->Validate();
+
+            if ($res[0] === false)
+                {
+                echo $res[1]."\n";
+                $aeform->PageBack();
+                }
+            else if (isset($params['done']) && $params['done']==1)
+            	{
+            	$finished = true;
+            	$results = $aeform->Dispose($returnid);
+            	}
+            }
+
+		if (! $finished)
+			{
+			$parms = array();
+    		$parms['form_name'] = $aeform->GetName();
+    		$parms['form_id'] = $aeform->GetId();
+    		$this->SendEvent('OnFormBuilderFormDisplay',$parms);
+
+        	echo $this->CreateFormStart($id, 'default', $returnid, 'post', 'multipart/form-data');
+        	echo $aeform->RenderForm($id, $params, $returnid);
+        	echo $this->CreateFormEnd();
+        	}
+        else
+        	{
+        	if ($results[0] == true)
+        		{
+				$parms = array();
+    			$parms['form_name'] = $aeform->GetName();
+    			$parms['form_id'] = $aeform->GetId();
+    			$this->SendEvent('OnFormBuilderFormSubmit',$parms);
+
+        		$ret = $aeform->GetAttr('redirect_page','-1');
+        		if ($ret != -1)
+        			{
+        			$this->RedirectContent($ret);
+        			}
+        		}
+        	else
+        		{
+				$parms = array();
+				$params['error']='';
+        		echo $this->Lang('submission_error');
+        		$show = $this->GetPreference('hide_errors',0);
+        		foreach ($results[1] as $thisRes)
+        			{
+					if ($show)
+						{
+        				echo $thisRes . '<br />';
+      				}
+        			$params['error'] .= $thisRes."\n";
+        			}
+    			$parms['form_name'] = $aeform->GetName();
+    			$parms['form_id'] = $aeform->GetId();
+    			$this->SendEvent('OnFormBuilderFormSubmitError',$parms);
+        		}
+        	}
+        echo $aeform->RenderFormFooter();
+
+
+
+	}
+*/
+
 
 	// For a given form, returns an array of response objects
 	function ListResponses($form_id, $sort_order='submitted')
