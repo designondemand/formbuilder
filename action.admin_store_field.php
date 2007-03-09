407121f815a8fe027f95825277e599ad39f73d08
calguy1000 <calguy1000@f3358df3-8306-0410-9a17-d642f5d875db>
2007-03-09 16:40:43 +0000

    Not Ready For Prime Time - More work on file uploads. - Added the module_id parameter to the module

--- action.admin_store_field.php
+++ action.admin_store_field.php
@@ -9,21 +9,21 @@
 if (!isset($gCms)) exit;
 if (! $this->CheckAccess()) exit;
 
-        $aeform = new fbForm($this, $params);
-        $aefield = $aeform->NewField($params);
-		$val = $aefield->AdminValidate();
-        if ($val[0])
-            {
-	        $aefield->PostAdminSubmitCleanup();
-            $aefield->Store(true);
-            $params['message']=$params['op'];
-            $this->DoAction('admin_add_edit_form', $id, $params);
-            }
-        else
-        	{
-            $aefield->LoadField($params);
-            $params['message'] = $val[1];
-			echo $aeform->AddEditField($id, $aefield, (isset($params['dispose_only'])?$params['dispose_only']:0), $returnid, isset($params['message'])?$params['message']:'');
-        	}
+$aeform = new fbForm($this, $params);
+$aefield = $aeform->NewField($params);
+$val = $aefield->AdminValidate();
+if ($val[0])
+  {
+    $aefield->PostAdminSubmitCleanup();
+    $aefield->Store(true);
+    $params['message']=$params['op'];
+    $this->DoAction('admin_add_edit_form', $id, $params);
+  }
+ else
+   {
+     $aefield->LoadField($params);
+     $params['message'] = $val[1];
+     echo $aeform->AddEditField($id, $aefield, (isset($params['dispose_only'])?$params['dispose_only']:0), $returnid, isset($params['message'])?$params['message']:'');
+   }
 
 ?>
