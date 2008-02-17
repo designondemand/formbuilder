3c25db643d7f3adefea64abf6fe590aca8e69b07
sjg <sjg@f3358df3-8306-0410-9a17-d642f5d875db>
2008-02-17 23:29:14 +0000

    Added email to site admin disposition

---
+++ trunk/classes/DispositionEmailSiteAdmin.class.php
@@ -0,0 +1,216 @@
+<?php
+/* 
+   FormBuilder. Copyright (c) 2005-2008 Samuel Goldstein <sjg@cmsmodules.com>
+   More info at http://dev.cmsmadesimple.org/projects/formbuilder
+   
+   A Module for CMS Made Simple, Copyright (c) 2008 by Ted Kulp (wishy@cmsmadesimple.org)
+  This project's homepage is: http://www.cmsmadesimple.org
+*/
+
+require_once('DispositionEmailBase.class.php');
+
+class fbDispositionEmailSiteAdmin extends fbDispositionEmailBase {
+
+	var $addressCount;
+	var $addressAdd;
+
+	function fbDispositionEmailSiteAdmin(&$form_ptr, &$params)
+	{
+       $this->fbDispositionEmailBase($form_ptr, $params);
+        $mod = &$form_ptr->module_ptr;
+		$this->Type = 'DispositionEmailSiteAdmin';
+		$this->DisplayInForm = true;
+		$this->IsDisposition = true;
+		$this->HasAddOp = false;
+		$this->HasDeleteOp = false;
+		$this->ValidationTypes = array(
+       		);
+	}
+
+
+	function GetFieldInput($id, &$params, $returnid)
+	{
+	    global $gCms;
+	    $userops =& $gCms->GetUserOperations();
+		$mod = &$this->form_ptr->module_ptr;
+
+		// why all this? Associative arrays are not guaranteed to preserve
+		// order, except in "chronological" creation order.
+		$sorted =array();
+		if ($this->GetOption('select_one','') != '')
+			{
+			$sorted[' '.$this->GetOption('select_one','')]='';
+			}
+		else
+			{
+			$sorted[' '.$mod->Lang('select_one')]='';
+			}
+			
+		if ($this->GetOption('restrict_to_group','0')=='1')
+			{
+			$userlist = $userops->LoadUsersInGroup($this->GetOption('group'));
+			}
+		else
+			{
+			$userlist = $userops->LoadUsers();
+			}
+		for($i=0;$i<count($userlist);$i++)
+			{
+			$name = $userlist[$i]->firstname.' '.$userlist[$i]->lastname;
+			if ($this->GetOption('show_username','0')=='1')
+				{
+				$name .=' ('.$userlist[$i]->username.')';
+				}
+			$sorted[$name]=($i+1);
+			}
+		return $mod->CreateInputDropdown($id, 'fbrp__'.$this->Id, $sorted, -1, $this->Value, 'id="'.$id. '_'.$this->Id.'"');
+	}
+
+
+
+    function StatusInfo()
+	{
+	    global $gCms;
+	    $groupops =& $gCms->GetGroupOperations();
+		$mod = &$this->form_ptr->module_ptr;
+        $ret = $this->TemplateStatus();
+		if ($this->GetOption('restrict_to_group','0')=='1')
+			{
+			$group = $groupops->LoadGroupByID($this->GetOption('group'));
+			if ($group && isset($group->name))
+				{
+				$ret .= ', '.$mod->Lang('restricted_to_group',$group->name);
+				}
+			}
+        return $ret;
+	}
+	
+	function PrePopulateAdminForm($formDescriptor)
+	{
+	    global $gCms;
+	    $groupops =& $gCms->GetGroupOperations();
+	    $groups = $groupops->LoadGroups();
+		$mod = &$this->form_ptr->module_ptr;
+
+		list($main,$adv) = $this->PrePopulateAdminFormBase($formDescriptor);
+		array_push($main,array($mod->Lang('title_select_one_message'),
+			$mod->CreateInputText($formDescriptor, 'fbrp_opt_select_one',
+			$this->GetOption('select_one',$mod->Lang('select_one')),25,128)));
+		array_push($main,array($mod->Lang('title_show_username'),
+				$mod->CreateInputHidden($formDescriptor,'fbrp_opt_show_username','0').
+				$mod->CreateInputCheckbox($formDescriptor, 'fbrp_opt_show_username', '1',
+				$this->GetOption('show_username','0'))));
+				
+
+		$items = array();
+		foreach ($groups as $thisGroup)
+			{
+			$items[$thisGroup->name]=$thisGroup->id;
+			}
+		
+		array_push($main,array($mod->Lang('title_restrict_to_group'),
+				$mod->CreateInputHidden($formDescriptor,'fbrp_opt_restrict_to_group','0').
+				$mod->CreateInputCheckbox($formDescriptor, 'fbrp_opt_restrict_to_group', '1',
+				$this->GetOption('restrict_to_group','0')).
+				$mod-> CreateInputDropdown($formDescriptor, 'fbrp_opt_group', $items, -1, $this->GetOption('group',''))
+				));
+				
+		return array('main'=>$main,'adv'=>$adv);
+	}
+
+	function PostPopulateAdminForm(&$mainArray, &$advArray)
+	{
+		$mod = &$this->form_ptr->module_ptr;
+		// remove the "hide css" field
+		$hideIndex = -1;
+		for ($i=0;$i<count($advArray);$i++)
+			{
+			if ($advArray[$i]->title == $mod->Lang('title_field_css_class'))
+				{
+				$hideIndex = $i;
+				}
+			}
+		if ($hideIndex != -1)
+			{
+			array_splice($advArray, $hideIndex,1);
+			}
+		if (count($advArray) == 0)
+			{
+			$advArray[0]->title = $mod->Lang('tab_advanced');
+			$advArray[0]->input = $mod->Lang('title_no_advanced_options');
+			}
+	}
+
+	function GetHumanReadableValue()
+	{
+	    global $gCms;
+	    $userops =& $gCms->GetUserOperations();
+		$mod = &$this->form_ptr->module_ptr;
+
+		if ($this->GetOption('restrict_to_group','0')=='1')
+			{
+			$userlist = $userops->LoadUsersInGroup($this->GetOption('group'));
+			}
+		else
+			{
+			$userlist = $userops->LoadUsers();
+			}
+		if (isset($userlist[$this->Value - 1]))
+			{
+			return $userlist[$this->Value - 1]->firstname . ' '. $userlist[$this->Value - 1]->lastname;
+			}
+		else
+			{
+			return $mod->Lang('unspecified');
+			}
+	}
+	
+	function DisposeForm($returnid)
+	{
+	    global $gCms;
+	    $userops =& $gCms->GetUserOperations();
+		$mod = &$this->form_ptr->module_ptr;
+
+		if ($this->GetOption('restrict_to_group','0')=='1')
+			{
+			$userlist = $userops->LoadUsersInGroup($this->GetOption('group'));
+			}
+		else
+			{
+			$userlist = $userops->LoadUsers();
+			}
+		$dest = array($userlist[$this->Value - 1]->email);
+		return $this->SendForm($dest,$this->GetOption('email_subject'));
+	}
+
+
+	function AdminValidate()
+    {
+		$mod = &$this->form_ptr->module_ptr;
+		$ret = true;
+		$message = "";
+		if (! preg_match(($mod->GetPreference('relaxed_email_regex','0')==0?$mod->email_regex:$mod->email_regex_relaxed),$this->GetOption('email_from_address')))
+			{
+    	       	$ret = false;
+                $message .= $mod->Lang('not_valid_email',$this->GetOption('email_from_address')) . '<br/>';
+			}
+        return array($ret,$message);
+    }
+    
+    function Validate()
+    {
+         $mod = &$this->form_ptr->module_ptr;
+         $result = true;
+         $message = '';
+
+         if ($this->Value == false)
+            {
+            $result = false;
+            $message .=
+$mod->Lang('must_specify_one_destination').'</br>';
+            }
+        return array($result,$message);
+    }
+
+}
+?>
