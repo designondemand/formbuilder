3251aa6b54d044e2507cc59c492a6840f962918b
simonschaufi <simonschaufi@f3358df3-8306-0410-9a17-d642f5d875db>
2009-04-04 09:41:17 +0000

    one @ob_clean() should be enough

--- trunk/action.admin_get_template.php
+++ trunk/action.admin_get_template.php
@@ -18,7 +18,6 @@ else
     }
 
     @ob_clean();
-    @ob_clean();
     header('Pragma: public');
     header('Expires: 0');
     header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
