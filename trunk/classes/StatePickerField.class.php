c5cb4e9a25c37a74bcc46e7ee0de82fdd5aecacb
simonschaufi <simonschaufi@f3358df3-8306-0410-9a17-d642f5d875db>
2009-04-04 23:00:00 +0000

    fbrp_ was missing

--- trunk/classes/StatePickerField.class.php
+++ trunk/classes/StatePickerField.class.php
@@ -78,7 +78,7 @@ class fbStatePickerField extends fbFieldBase {
 		  $this->SetValue($this->GetOption('default',''));
 		  }
 
-		return $mod->CreateInputDropdown($id, 'fbrp__'.$this->Id, $this->States, -1, $this->Value,'id="'.$id. '_'.$this->Id.'" '.$js);
+		return $mod->CreateInputDropdown($id, 'fbrp__'.$this->Id, $this->States, -1, $this->Value, 'id="'.$id. 'fbrp__'.$this->Id.'" '.$js);
 	}
 
 	function PrePopulateAdminForm($formDescriptor)
