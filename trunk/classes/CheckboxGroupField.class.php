f1aa34d3cf93138a65190151fdde522c8a3c02f0
Stikki <Stikki@f3358df3-8306-0410-9a17-d642f5d875db>
2010-05-11 19:15:13 +0000

    TextFieldExpandabel: adding link to sibling option, adding hide controls option. CheckboxGroupField: Adding include label to output option -thanks to moonoo about idea.

--- trunk/classes/CheckboxGroupField.class.php
+++ trunk/classes/CheckboxGroupField.class.php
@@ -12,6 +12,7 @@ class fbCheckboxGroupField extends fbFieldBase {
 	var $boxCount;
 	var $boxAdd;
 	
+	// Constructor
 	function fbCheckboxGroupField(&$form_ptr, &$params)
 	{
         $this->fbFieldBase($form_ptr, $params);
@@ -21,30 +22,30 @@ class fbCheckboxGroupField extends fbFieldBase {
 		$this->HasAddOp = true;
 		$this->HasDeleteOp = true;
 		$this->NonRequirableField = false;
-		$this->ValidationTypes = array(
-            );
+		$this->ValidationTypes = array();
         $this->boxAdd = 0;
         $this->hasMultipleFormComponents = true;
         $this->sortable = false;
 	}
 
+	// Count how many boxes we have
 	function countBoxes()
 	{
-			$tmp = $this->GetOptionRef('box_name');
-			if (is_array($tmp))
-				{
-	        	$this->boxCount = count($tmp);
-	        	}
-	        elseif ($tmp !== false)
-	        	{
-	        	$this->boxCount = 1;
-	        	}
-	        else
-	        	{
-	        	$this->boxCount = 0;
-	        	}
+		$tmp = $this->GetOptionRef('box_name');
+		if (is_array($tmp)) {
+		
+	        $this->boxCount = count($tmp);
+		
+		} elseif ($tmp !== false) {
+	        
+			$this->boxCount = 1;
+	    } else {
+			
+			$this->boxCount = 0;
+	    }
 	}
 
+	// Return status
     function StatusInfo()
 	{
         $mod = $this->form_ptr->module_ptr;
@@ -53,18 +54,21 @@ class fbCheckboxGroupField extends fbFieldBase {
 		 return $ret;
 	}
 
+	// Get add button
 	function GetOptionAddButton()
 	{
 		$mod = $this->form_ptr->module_ptr;
 		return $mod->Lang('add_checkboxes');
 	}
 
+	// Get delete button
 	function GetOptionDeleteButton()
 	{
 		$mod = $this->form_ptr->module_ptr;
 		return $mod->Lang('delete_checkboxes');
 	}
 
+	// Get input
 	function GetFieldInput($id, &$params, $returnid)
 	{
 		$mod = $this->form_ptr->module_ptr;
@@ -109,55 +113,79 @@ class fbCheckboxGroupField extends fbFieldBase {
 		return $fieldDisp;
 	}
 
+	// Get Human readable value
 	function GetHumanReadableValue($as_string=true)
 	{
 		$form = $this->form_ptr;
+		
+		// Init names
 		$names = $this->GetOptionRef('box_name');
-		if (! is_array($names))
-			{
+		if (! is_array($names)) {
+		
 				$names = array($names);
-			}		
+		}
+		
+		// Init checked boxes
 		$checked = $this->GetOptionRef('box_checked');
-		if (! is_array($checked))
-			{
-				$checked = array($checked);
-			}
+		if (! is_array($checked)) {
+		
+			$checked = array($checked);
+		}
+		
+		// Init unchecked boxes
 		$unchecked = $this->GetOptionRef('box_unchecked');
-		if (! is_array($unchecked))
-			{
-				$unchecked = array($unchecked);
-			}
+		if (! is_array($unchecked)) {
+		
+			$unchecked = array($unchecked);
+		}
+		
+		//$fieldLabels = array();
 		$fieldRet = array();
-		for ($i=1;$i<=count($names);$i++)
-			{
-				if ($this->FindArrayValue($i) === false)
-					{
-						if ($this->GetOption('no_empty','0') != '1' && isset($unchecked[$i-1]) && trim($unchecked[$i-1]) != '' )
-							{
-							$fieldRet[] = $unchecked[$i-1];
-							}
-					}
-				else
-					{
-						if( isset($checked[$i-1]) && trim($checked[$i-1]) != '' )
-							$fieldRet[] = $checked[$i-1];
-					}
+		for ($i=1;$i<=count($names);$i++) {
+		
+			if ($this->FindArrayValue($i) === false) {
+			
+				if ($this->GetOption('no_empty','0') != '1' && isset($unchecked[$i-1]) && trim($unchecked[$i-1]) != '' ) {
+				
+					$fieldRet[$names[$i-1]] = $unchecked[$i-1];
+				}
+			} else {
+				
+				if( isset($checked[$i-1]) && trim($checked[$i-1]) != '' ) $fieldRet[$names[$i-1]] = $checked[$i-1];
 			}
-		if ($as_string)
-			{
+		}
+		
+		if ($as_string) {
+			
+			// Check if we should include labels
+			if($this->GetOption('include_labels','0') == true) {
+			
+				$output = '';
+				foreach($fieldRet as $key=>$value) {
+				
+					$output .= $key.': '.$value.$form->GetAttr('list_delimiter',',');
+				
+				}
+			
+				$output = substr($output,0,strlen($output)-1);
+				return $output;
+			}
+			
 			return join($form->GetAttr('list_delimiter',','),$fieldRet);
-			}
-		else
-			{
+			
+		} else {
+		
 			return $fieldRet;
-			}
+		}
 	}
 
+	// Add action
 	function DoOptionAdd(&$params)
 	{
 		$this->boxAdd = 1;
 	}
 
+	// Delete action
 	function DoOptionDelete(&$params)
 	{
 		$delcount = 0;
@@ -174,7 +202,7 @@ class fbCheckboxGroupField extends fbFieldBase {
 			}
 	}
 
-
+	// Populate tabs
 	function PrePopulateAdminForm($formDescriptor)
 	{
 		$mod = $this->form_ptr->module_ptr;
@@ -186,51 +214,56 @@ class fbCheckboxGroupField extends fbFieldBase {
 			$this->boxCount += $this->boxAdd;
 			$this->boxAdd = 0;
 			}
-		$boxes = '<table class="module_fb_table"><tr><th>'.$mod->Lang('title_checkbox_label').'</th><th>'.
+			$boxes = '<table class="module_fb_table pagetable" cellpadding="0" cellspacing="0"><thead><tr><th>'.
+			$mod->Lang('title_checkbox_label').'</th><th>'.
 			$mod->Lang('title_checked_value').'</th><th>'.
 			$mod->Lang('title_unchecked_value').'</th><th>'.
 			$mod->Lang('title_default_set').'</th><th>'.
-			$mod->Lang('title_delete').'</th></tr>';
+			$mod->Lang('title_delete').'</th></tr></thead><tbody>';
 
-
+		$rowclass = 'row1';
 		for ($i=0;$i<($this->boxCount>1?$this->boxCount:1);$i++)
 			{
-			$boxes .= '<tr><td>'.
+			$boxes .= '<tr class="'.$rowclass.'"><td>'.
             		$mod->CreateInputText($formDescriptor, 'fbrp_opt_box_name[]',$this->GetOptionElement('box_name',$i),25,128).
             		'</td><td>'.
             		$mod->CreateInputText($formDescriptor, 'fbrp_opt_box_checked[]',$this->GetOptionElement('box_checked',$i),25,128).
             		'</td><td>'.
             		$mod->CreateInputText($formDescriptor, 'fbrp_opt_box_unchecked[]',$this->GetOptionElement('box_unchecked',$i),25,128).
             		'</td><td>'.            		    
-            		$mod->CreateInputDropdown($formDescriptor, 'fbrp_opt_box_is_set[]', $yesNo, -1, $this->GetOptionElement('box_is_set',$i)).
-            		
-            		
+            		$mod->CreateInputDropdown($formDescriptor, 'fbrp_opt_box_is_set[]', $yesNo, -1, $this->GetOptionElement('box_is_set',$i)).	
             		'</td><td>'.
-//CreateInputCheckbox($id, $name, $value='', $selectedvalue='',
             		$mod->CreateInputCheckbox($formDescriptor, 'fbrp_del_'.$i, $i,-1).
              		'</td></tr>';
+					
+			($rowclass == 'row1'?$rowclass='row2':$rowclass='row1');
 			}
-		$boxes .= '</table>';
+		$boxes .= '</tbody></table>';
+		
 		$main = array(
-			array($mod->Lang('title_dont_submit_unchecked'),
-				$mod->CreateInputHidden($formDescriptor,'fbrp_opt_no_empty','0').
-					$mod->CreateInputCheckbox($formDescriptor, 'fbrp_opt_no_empty','1',$this->GetOption('no_empty','0')).
-					$mod->Lang('title_dont_submit_unchecked_help'),
-	           ),
+			array($mod->Lang('title_dont_submit_unchecked'),$mod->CreateInputHidden($formDescriptor,'fbrp_opt_no_empty','0').
+						$mod->CreateInputCheckbox($formDescriptor, 'fbrp_opt_no_empty','1',$this->GetOption('no_empty','0')).$mod->Lang('title_dont_submit_unchecked_help')),
 			array($mod->Lang('title_checkbox_details'),$boxes)
 		);
-		$adv = array();
+		
+		$adv = array(
+			array($mod->Lang('title_field_includelabels'),$mod->CreateInputHidden($formDescriptor,'fbrp_opt_include_labels','0').
+						$mod->CreateInputCheckbox($formDescriptor, 'fbrp_opt_include_labels','1',$this->GetOption('include_labels','0')).$mod->Lang('title_field_includelabels_help'))
+		);
+		
 		return array('main'=>$main,'adv'=>$adv);
 	}
 
+	// Modify field admin before showing it
 	function PostPopulateAdminForm(&$mainArray, &$advArray)
 	{
 		$mod = $this->form_ptr->module_ptr;
+		
 		// remove the "required" field, since this can only be done via validation
-      $this->RemoveAdminField($mainArray, $mod->Lang('title_field_required'));
+		$this->RemoveAdminField($mainArray, $mod->Lang('title_field_required'));
 	}
 
-
+	// Before admin submit do cleanup
 	function PostAdminSubmitCleanup()
 	{
 		$names = $this->GetOptionRef('box_name');
@@ -250,6 +283,7 @@ class fbCheckboxGroupField extends fbFieldBase {
 			}
 		$this->countBoxes();
 	}
-}
+	
+} // End of Class
 
 ?>
