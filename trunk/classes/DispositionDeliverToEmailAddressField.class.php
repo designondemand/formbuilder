f4def7ca8003f37bef86c831da931dfb3afda143
Foaly* <Foaly*@f3358df3-8306-0410-9a17-d642f5d875db>
2013-08-30 10:46:05 +0000

    introduced consistent file formatting and php5 to some classes

--- trunk/classes/DispositionDeliverToEmailAddressField.class.php
+++ trunk/classes/DispositionDeliverToEmailAddressField.class.php
@@ -1,48 +1,45 @@
 <?php
-/* 
-   FormBuilder. Copyright (c) 2005-2006 Samuel Goldstein <sjg@cmsmodules.com>
-   More info at http://dev.cmsmadesimple.org/projects/formbuilder
-   
-   A Module for CMS Made Simple, Copyright (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
-  This project's homepage is: http://www.cmsmadesimple.org
-*/
+/*
+ * FormBuilder. Copyright (c) 2005-2006 Samuel Goldstein <sjg@cmsmodules.com>
+ * More info at http://dev.cmsmadesimple.org/projects/formbuilder
+ *
+ * A Module for CMS Made Simple, Copyright (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
+ * This project's homepage is: http://www.cmsmadesimple.org
+ */
+
 require_once(dirname(__FILE__).'/DispositionEmailBase.class.php');
 
 class fbDispositionDeliverToEmailAddressField extends fbDispositionEmailBase {
 
-	function fbDispositionDeliverToEmailAddressField(&$form_ptr, &$params)
+	function __construct(&$form_ptr, &$params)
 	{
-      $this->fbDispositionEmailBase($form_ptr, $params);
-      $mod = $form_ptr->module_ptr;
+		parent::__construct($form_ptr, $params);
+		$mod = $form_ptr->module_ptr;
 		$this->Type = 'DispositionDeliverToEmailAddressField';
-      $this->IsDisposition = true;
+		$this->IsDisposition = true;
 		$this->DisplayInForm = true;
-		$this->ValidationTypes = array(
-            $mod->Lang('validation_email_address')=>'email',
-            );
-      $this->ValidationType = 'email';
-	   $this->modifiesOtherFields = false;
+		$this->ValidationTypes = array($mod->Lang('validation_email_address')=>'email',);
+		$this->ValidationType = 'email';
+		$this->modifiesOtherFields = false;
 	}
 
 	function GetFieldInput($id, &$params, $returnid)
 	{
 		$mod = $this->form_ptr->module_ptr;
 		$js = $this->GetOption('javascript','');
-		return $mod->fbCreateInputText($id, 'fbrp__'.$this->Id,
-			htmlspecialchars($this->Value, ENT_QUOTES),
-           25,128,$js.$this->GetCSSIdTag());
+		return $mod->fbCreateInputText($id, 'fbrp__'.$this->Id,htmlspecialchars($this->Value, ENT_QUOTES),25,128,$js.$this->GetCSSIdTag());
 	}
 
 	function DisposeForm($returnid)
 	{
-      if ($this->HasValue() != false)
-         {
-		   return $this->SendForm($this->Value,$this->GetOption('email_subject'));
-		   }
+		if ($this->HasValue() != false)
+		{
+			return $this->SendForm($this->Value,$this->GetOption('email_subject'));
+		}
 		else
-		   {
-         return array(true,'');
-         }
+		{
+			return array(true,'');
+		}
 	}
 
 
@@ -60,17 +57,17 @@ class fbDispositionDeliverToEmailAddressField extends fbDispositionEmailBase {
 	function Validate()
 	{
 
-  		$this->validated = true;
-  		$this->validationErrorText = '';
+		$this->validated = true;
+		$this->validationErrorText = '';
 		$result = true;
 		$message = '';
 		$mod = $this->form_ptr->module_ptr;
-      if ($this->Value !== false &&
-            ! preg_match(($mod->GetPreference('relaxed_email_regex','0')==0?$mod->email_regex:$mod->email_regex_relaxed), $this->Value))
-         {
-         $this->validated = false;
-         $this->validationErrorText = $mod->Lang('please_enter_an_email',$this->Name);
-         }
+		if ($this->Value !== false &&
+			! preg_match(($mod->GetPreference('relaxed_email_regex','0')==0?$mod->email_regex:$mod->email_regex_relaxed), $this->Value))
+		{
+			$this->validated = false;
+			$this->validationErrorText = $mod->Lang('please_enter_an_email',$this->Name);
+		}
 
 		return array($this->validated, $this->validationErrorText);
 	}
--- trunk/classes/DispositionFromEmailAddressField.class.php
+++ trunk/classes/DispositionFromEmailAddressField.class.php
@@ -1,28 +1,29 @@
 <?php
-/* 
-   FormBuilder. Copyright (c) 2005-2006 Samuel Goldstein <sjg@cmsmodules.com>
-   More info at http://dev.cmsmadesimple.org/projects/formbuilder
-   
-   A Module for CMS Made Simple, Copyright (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
-  This project's homepage is: http://www.cmsmadesimple.org
-*/
+/*
+ * FormBuilder. Copyright (c) 2005-2006 Samuel Goldstein <sjg@cmsmodules.com>
+ * More info at http://dev.cmsmadesimple.org/projects/formbuilder
+ *
+ * A Module for CMS Made Simple, Copyright (c) 2006 by Ted Kulp (wishy@cmsmadesimple.org)
+ * This project's homepage is: http://www.cmsmadesimple.org
+ */
+
 require_once(dirname(__FILE__).'/DispositionEmailBase.class.php');
 
 class fbDispositionFromEmailAddressField extends fbDispositionEmailBase {
 
-	function fbDispositionFromEmailAddressField(&$form_ptr, &$params)
+	function __construct(&$form_ptr, &$params)
 	{
-      $this->fbDispositionEmailBase($form_ptr, $params);
-      $mod = $form_ptr->module_ptr;
+		parent::__construct($form_ptr, $params);
+		$mod = $form_ptr->module_ptr;
 		$this->Type = 'DispositionFromEmailAddressField';
-      $this->IsDisposition = true;
+		$this->IsDisposition = true;
 		$this->DisplayInForm = true;
 		$this->ValidationTypes = array(
 			$mod->Lang('validation_none')=>'none',
-            $mod->Lang('validation_email_address')=>'email',
-            );
-	$this->modifiesOtherFields = true;
-	$this->NonRequirableField = false;	
+			$mod->Lang('validation_email_address')=>'email',
+		);
+		$this->modifiesOtherFields = true;
+		$this->NonRequirableField = false;
 	}
 
 	function GetFieldInput($id, &$params, $returnid)
@@ -30,15 +31,15 @@ class fbDispositionFromEmailAddressField extends fbDispositionEmailBase {
 		$mod = $this->form_ptr->module_ptr;
 		$js = $this->GetOption('javascript','');
 		$retstr = '<input type="text" name="'.$id.'fbrp__'.$this->Id.'[]" '.
-			$this->GetCSSIdTag('_1').' value="'.htmlspecialchars($this->Value[0], ENT_QUOTES).
+		$this->GetCSSIdTag('_1').' value="'.htmlspecialchars($this->Value[0], ENT_QUOTES).
 			'" size="25" maxlength="128" '.$js.'/>';
- 		if ($this->GetOption('send_user_copy','n') == 'c')
-			{
+		if ($this->GetOption('send_user_copy','n') == 'c')
+		{
 			$retstr .= $mod->CreateInputCheckbox($id, 'fbrp__'.$this->Id.'[]', 1,
-					0,$this->GetCSSIdTag('_2'),'email');
+			0,$this->GetCSSIdTag('_2'),'email');
 			$retstr .= '<label for="'.$this->GetCSSId('_2').'" class="label">'.$this->GetOption('send_user_label',
-				$mod->Lang('title_send_me_a_copy')).'</label>';
-			}
+			$mod->Lang('title_send_me_a_copy')).'</label>';
+		}
 		return $retstr;
 	}
 
@@ -46,75 +47,75 @@ class fbDispositionFromEmailAddressField extends fbDispositionEmailBase {
 	{
 		$alias = $this->GetAlias();
 		if (empty($alias))
-			{
+		{
 			$cssid = 'fbrp__'.$this->Id;
-			}
+		}
 		else
-			{
+		{
 			$cssid = $alias;
-			}
+		}
 
 		if (empty($suffix))
-			{
+		{
 			$cssid .= '_1';
-			}
+		}
 		else
-			{
+		{
 			$cssid .= $suffix;
-			}
+		}
 		return $cssid;
 	}
 
-   function HasValue($deny_blank_responses=false)
-   {
-   return ($this->Value[0] !== false && !empty($this->Value[0]));
-   }
+	function HasValue($deny_blank_responses=false)
+	{
+		return ($this->Value[0] !== false && !empty($this->Value[0]));
+	}
 
-  	function GetValue()
-  	{
-    	return $this->Value[0];
-  	}
+	function GetValue()
+	{
+		return $this->Value[0];
+	}
 
-   function SetValue($valStr)
-   {
-      if (! is_array($valStr))
-         {
-         $this->Value = array($valStr);
-         }
-      else
-         {
-         $this->Value = $valStr;
-         }
-   }
+	function SetValue($valStr)
+	{
+		if (! is_array($valStr))
+		{
+			$this->Value = array($valStr);
+		}
+		else
+		{
+			$this->Value = $valStr;
+		}
+	}
 
 	function GetHumanReadableValue($as_string=true)
 	{
 		if (is_array($this->Value))
-			{
+		{
 			return $this->Value[0];
-			}
+		}
 		else
-			{
+		{
 			return $this->Value;
-			}
+		}
 	}
 
 	function DisposeForm($returnid)
 	{
-      if ($this->HasValue() != false && 
+		if ($this->HasValue() != false &&
 			(
 				$this->GetOption('send_user_copy','n') == 'a'
 				||
 				($this->GetOption('send_user_copy','n') == 'c' && isset($this->Value[1]) && $this->Value[1] == 1)
 			)
-		 )
-			{
-			return $this->SendForm($this->Value[0],$this->GetOption('email_subject'));
-			}
-	  else
+		)
 		{
-        return array(true,'');
-        }
+			return $this->SendForm($this->Value[0],$this->GetOption('email_subject'));
+		}
+		else
+		{
+			return array(true,'');
+		}
 	}
 
 
@@ -152,45 +153,44 @@ class fbDispositionFromEmailAddressField extends fbDispositionEmailBase {
 		$others = $this->form_ptr->GetFields();
 		$htm = $this->GetOption('headers_to_modify','f');
 		if ($this->Value !== false)
-			{
+		{
 			for($i=0;$i<count($others);$i++)
-				{
+			{
 				$replVal = '';
 				if ($others[$i]->IsDisposition() &&
-               is_subclass_of($others[$i],'fbDispositionEmailBase'))
-					{
+				is_subclass_of($others[$i],'fbDispositionEmailBase'))
+				{
 					if ($htm == 'f' || $htm == 'b')
-                  {
-					    $others[$i]->SetOption('email_from_address',$this->Value[0]);
-					   }
-               if ($htm == 'r' || $htm == 'b')
-                  {
-					    $others[$i]->SetOption('email_reply_to_address',$this->Value[0]);
-                  }
+					{
+						$others[$i]->SetOption('email_from_address',$this->Value[0]);
+					}
+					if ($htm == 'r' || $htm == 'b')
+					{
+						$others[$i]->SetOption('email_reply_to_address',$this->Value[0]);
 					}
 				}
 			}
+		}
 	}
 
 	function Validate()
 	{
 
-  		$this->validated = true;
-  		$this->validationErrorText = '';
+		$this->validated = true;
+		$this->validationErrorText = '';
 		$result = true;
 		$message = '';
 		$mod = $this->form_ptr->module_ptr;
 		if ($this->ValidationType != 'none')
+		{
+			if ($this->Value !== false &&
+				!preg_match(($mod->GetPreference('relaxed_email_regex','0')==0?$mod->email_regex:$mod->email_regex_relaxed), $this->Value[0]))
 			{
-		      if ($this->Value !== false &&
-		            ! preg_match(($mod->GetPreference('relaxed_email_regex','0')==0?$mod->email_regex:$mod->email_regex_relaxed), $this->Value[0]))
-		         {
-		         $this->validated = false;
-		         $this->validationErrorText = $mod->Lang('please_enter_an_email',$this->Name);
-		         }
+				$this->validated = false;
+				$this->validationErrorText = $mod->Lang('please_enter_an_email',$this->Name);
 			}
+		}
 		return array($this->validated, $this->validationErrorText);
 	}
 }
-
 ?>
